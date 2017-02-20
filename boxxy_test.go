b42ced07eb5af5bece266f76e3a7f0ae76ee0730
itsMontoya <josh@montoya.io>
2017-02-20 12:12:10 -0800

    Found issue with how I returned the populated slice. Nice catch by @OneOfOne!

--- boxxy_test.go
+++ boxxy_test.go
@@ -160,8 +160,8 @@ func populatedBoxxy(n int) (b *Boxxy) {
 	return
 }
 
-func populatedSlice(n int) (s []int) {
-	s = make([]int, n)
+func populatedSlice(n int) (s []interface{}) {
+	s = make([]interface{}, n)
 
 	for i := 0; i < n; i++ {
 		s[i] = i
